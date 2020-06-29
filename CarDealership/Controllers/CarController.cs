a99e1fd2bd77b26c72fa18c225fcc53b25b304ee
Christina Lambert <clambert0615@gmail.com>
2020-06-29 12:36:01 -0400

    Added front end styling and images

--- CarDealership/Controllers/CarController.cs
+++ CarDealership/Controllers/CarController.cs
@@ -24,6 +24,37 @@ namespace CarDealership.Controllers
             List<Cars> carList = await _car.GetCars(Make, Model, Year, Color);
             return View(carList);
         }
-       
+        public IActionResult AddCarInfo()
+        {
+            return View();
+        }
+        public async Task<IActionResult> AddNewCar(Cars newCar)
+        {
+            await _car.AddCar(newCar);
+            return RedirectToAction("SearchIndex");
+        }
+
+        public async Task<IActionResult> DeleteCar(int id)
+        {
+           _car.DeleteCar(id);
+             return RedirectToAction("SearchIndex");
+        }
+       [HttpGet]
+        public async Task<IActionResult> UpdateCar(int id)
+        {
+            Cars foundcar = await _car.GetCar(id);
+            return View(foundcar);
+        }
+        [HttpPost]
+        public async Task<IActionResult> UpdateCar( Cars updatedcar)
+        {
+            Cars oldcar = await _car.GetCar(updatedcar.Id);
+            oldcar.Model = updatedcar.Model;
+            oldcar.Make = updatedcar.Make;
+            oldcar.Year = updatedcar.Year;
+            oldcar.Color = updatedcar.Color;
+            _car.UpdateCar(oldcar);
+            return RedirectToAction("SearchIndex");
+        }
     }
 }
--- CarDealership/Models/CarDAL.cs
+++ CarDealership/Models/CarDAL.cs
@@ -26,6 +26,34 @@ namespace CarDealership.Models
             return cars;
         }
 
-    
+        public async Task<Cars> AddCar(Cars newCar)
+        {
+            //employee.id = 1;
+            var client = GetHttpClient();
+            var response = await client.PostAsJsonAsync($"api/carsapi", newCar);
+            var carResult = await response.Content.ReadAsAsync<Cars>();
+            return carResult;
+        }
+
+        public async void DeleteCar(int id)
+        {
+            var client = GetHttpClient();
+            var response = await client.DeleteAsync($"api/carsapi/{id}");
+        }
+
+        public async void UpdateCar(Cars updatedCar)
+        {
+            var client = GetHttpClient();
+            var response = await client.PutAsJsonAsync($"api/carsapi", updatedCar);
+        }
+
+        public async Task<Cars> GetCar(int id)
+        {
+            var client = GetHttpClient();
+            var response = await client.GetAsync($"api/carsapi/{id}");
+            //install-package Microsoft.AspNet.WebAPI.Client
+            var car = await response.Content.ReadAsAsync<Cars>();
+            return car;
+        }
     }
 }
